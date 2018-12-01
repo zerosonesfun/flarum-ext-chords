b4c5c94e2cf98df9063bb3db1d37cd94ff72a2d5
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2018-12-01 16:10:58 -0500

    Update extend.php

--- extend.php
+++ extend.php
@@ -13,21 +13,20 @@ use Flarum\Extend;
 use Flarum\Frontend\Document;
 use s9e\TextFormatter\Configurator;
 
-return (new Extend\Formatter)
-    ->configure(function (Configurator $config) {
-        $config->BBCodes->addCustom(
-        '[chord]{TEXT1}[/chord]',
-        '<span class="chord">{TEXT1}</span>'
-        );
-        $config->BBCodes->addCustom(
-        '[song]{TEXT2}[/song]',
-        '<p class="song show-chords">{TEXT2}</p>'
-        );
-    });
-
 return [
     (new Extend\Frontend('forum'))
         ->content(function (Document $document) {
             $document->head[] = '<link rel="stylesheet" type="text/css" href="/assets/extensions/zerosonesfun-chords/styles.css">';
+        }),
+    (new Extend\Formatter)
+        ->configure(function (Configurator $config) {
+            $config->BBCodes->addCustom(
+                '[chord]{TEXT1}[/chord]',
+                '<span class="chord">{TEXT1}</span>'
+            );
+            $config->BBCodes->addCustom(
+                '[song]{TEXT2}[/song]',
+                '<p class="song show-chords">{TEXT2}</p>'
+            );
         })
 ];
