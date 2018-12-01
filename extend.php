a3bbe581841f45a4eaf652c39c9b9bd252220885
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2018-12-01 15:34:01 -0500

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,9 +1,21 @@
 <?php
 
-use Flarum\Extend;
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-return [
-(new Extend\Frontend('forum'))
-    ->js(__DIR__.'/assets/extensions/zerosonesfun-chords/scripts.js')
-    ->css(__DIR__.'/assets/extensions/zerosonesfun-chords/styles.css')
-];
+use Flarum\Extend;
+use s9e\TextFormatter\Configurator;
+
+return (new Extend\Formatter)
+    ->configure(function (Configurator $config) {
+        $config->BBCodes->addCustom(
+        '[chord]{TEXT}[/chord]',
+        '<span class="test">{TEXT}</span>'
+        );
+    });
