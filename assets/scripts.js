0140f828520219a0397175742541c5b1c9364ef5
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2018-12-01 15:39:52 -0500

    Delete scripts.js

--- assets/scripts.js
+++
@@ -1,7 +0,0 @@
-document.body.innerHTML = document.body.innerHTML.replace(/{songbegin}/g, '<p class="song show-chords">');
-
-document.body.innerHTML = document.body.innerHTML.replace(/{songend}/g, '</p>');
-
-document.body.innerHTML = document.body.innerHTML.replace(/{{/g, '<span class="chord">');
-
-document.body.innerHTML = document.body.innerHTML.replace(/}}/g, '</span>');
